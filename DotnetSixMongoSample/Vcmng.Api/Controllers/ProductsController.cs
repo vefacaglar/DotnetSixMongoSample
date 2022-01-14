1e0b0b760e4b36b3a411e31f3c893abb5aff064d
Vefa Çağlar Özcan <vefacaglarozcan@gmail.com>
2022-01-14 19:24:46 +0300

    initial project

---
+++ DotnetSixMongoSample/Vcmng.Api/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vcmng.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
